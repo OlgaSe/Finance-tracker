ece6ad9bd07d85499c259eeb56acb51d743cc1d3
OlgaSe <olga.tka4eva@gmail.com>
2021-09-15 09:54:47 -0700

    finance tracking app

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+    return (
+    <div>
+        <div>March, 29 2020</div>
+        <div>
+            <h2>Car Insurance</h2>
+            <div>$245.9</div>
+        </div>
+    </div>
+    );
+}
+
+export default ExpenseItem;
